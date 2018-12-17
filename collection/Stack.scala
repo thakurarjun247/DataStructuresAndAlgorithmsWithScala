6d3232ff1c08948ff918f5fe30346a7d648054e8
Arjun Thakur <thakurarjun247@gmail.com>
2018-12-17 16:50:54 +0530

    level order, top view, bottom view in BST, added some methods in Stack

--- collection/Stack.scala
+++ collection/Stack.scala
@@ -4,10 +4,14 @@ import scala.collection.mutable
 
 object Stack extends App {
   val stack = mutable.Stack[Int]()
+  //for stack there are no += or ++=
   stack.size
   stack.push(1)
+  stack.pushAll(Seq(1,2,3))
+
   //remove top element or throw exception
   println(stack.pop())
+  println(stack.clear)
 
   //head and top are same, return top elem but don't delete it, readonly
   //throw exception if empty
--- ds/tree/BinarySearchTree.scala
+++ ds/tree/BinarySearchTree.scala
@@ -1,11 +1,19 @@
 package ds.tree
 
+import scala.collection.mutable
 import scala.io.StdIn._
 
-class Node(var key: Int, var left: Node = null, var right: Node = null)
+class Node(var key: Int, var left: Node = null, var right: Node = null, var hd: Int = 0)
 
 object BinarySearchTree extends App {
   var root: Node = null
+  create()
+  println("\nInorder traversal...")
+  inorder(root)
+  levelOrder(root)
+  bottomView(root)
+  topView(root)
+
   def insert(root: Node, key: Int): Node = {
     if (root == null) return new Node(key)
     else {
@@ -14,6 +22,7 @@ object BinarySearchTree extends App {
     }
     root
   }
+
   def inorder(root: Node): Unit = {
     if (root != null) {
       inorder(root.left)
@@ -21,18 +30,84 @@ object BinarySearchTree extends App {
       inorder(root.right)
     }
   }
+
   def create() = {
-    println("# nodes in tree")
+    println("\nCreating BST..")
+    println("Enter # nodes in tree")
     var n = readInt
     while (n != 0) {
-      println("enter node ")
+      println("Enter node value: ")
       root = insert(root, readInt())
       n = n - 1
     }
   }
 
-  println("creating..")
-  create()
-  println("traversing..")
-  inorder(root)
+  def levelOrder(root: Node) =
+    if (root != null) {
+      println("\nLevel order traversal...")
+      val q = new mutable.Queue[Node]()
+      q.enqueue(root)
+      while (q.size != 0) {
+        val node = q.dequeue()
+        print(s"${node.key} ")
+        if (node.left != null) q.enqueue(node.left)
+        if (node.right != null) q.enqueue(node.right)
+      }
+    }
+
+  def bottomView(root: Node) = {
+    println("\nBottom view..")
+    //keep them ordered
+    val map = mutable.TreeMap[Int, Int]()
+    if (root != null) {
+      val q = new mutable.Queue[Node]()
+      q.enqueue(root)
+      while (q.size != 0) {
+        var hd = 0
+        val node = q.dequeue()
+        //notice: the only diff b/n top and bottom view is to replace or not replae map entries
+        map += (node.hd -> node.key)
+        if (node.left != null) {
+          node.left.hd = node.hd - 1
+          q.enqueue(node.left)
+        }
+        if (node.right != null) {
+          node.right.hd = node.hd + 1
+          q.enqueue(node.right)
+        }
+      }
+    }
+    map
+      .foreach(pair => print(s" ${pair._2}"))
+
+  }
+
+  def topView(root: Node) = {
+    println("\nTop view..")
+    //keep them ordered
+    val map = mutable.TreeMap[Int, Int]()
+    if (root != null) {
+      val q = new mutable.Queue[Node]()
+      q.enqueue(root)
+      while (q.size != 0) {
+        var hd = 0
+        val node = q.dequeue()
+        //notice: the only diff b/n top and bottom view is to replace or not replae map entries
+
+        if (!map.contains(node.hd)) map += (node.hd -> node.key)
+        if (node.left != null) {
+          node.left.hd = node.hd - 1
+          q.enqueue(node.left)
+        }
+        if (node.right != null) {
+          node.right.hd = node.hd + 1
+          q.enqueue(node.right)
+        }
+      }
+    }
+    map
+      .foreach(pair => print(s" ${pair._2}"))
+
+  }
+
 }
