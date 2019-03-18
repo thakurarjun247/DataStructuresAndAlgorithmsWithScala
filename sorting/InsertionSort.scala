b6e0de811d302d6d6234b1c5c8cc5ca290fa62d2
Arjun Thakur <thakurarjun247@gmail.com>
2019-03-18 10:29:00 +0530

    added insertion sort

---
+++ sorting/InsertionSort.scala
@@ -0,0 +1,18 @@
+package sorting
+object InsertionSort extends App {
+  val a = Array(5, 2, 4, 6, 1, 3)
+  inSort(a).foreach(print)
+  def inSort(a: Array[Int]) = {
+    for (j <- 1 to a.length - 1) {
+      var i = j
+      while (i > 0 && a(i) < a(i - 1)) {
+        val key = a(i)
+        a(i) = a(i - 1)
+        a(i - 1) = key
+        i = i - 1
+      }
+    }
+    a
+  }
+}
+//D:\AllScala\src\sorting
