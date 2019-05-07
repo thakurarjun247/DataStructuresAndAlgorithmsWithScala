4ab234fc579f2f90d839ba9b553b5ef40e2219c0
Arjun Thakur <thakurarjun247@gmail.com>
2019-05-07 13:26:56 +0530

    system design doc

--- datastructure/mutable/stack/StackApp.scala
+++ datastructure/mutable/stack/StackApp.scala
@@ -2,5 +2,5 @@ package datastructure.mutable.stack
 object StackApp extends App {
   val stack = new Stack[Int]
   0 to 5 foreach (stack.push)
-  0 to 6 foreach (item => println(stack.pop))
+  0 to 4 foreach (item => println(stack.pop))
 }
